218aa8cfa52b1ff65a5199d16d782b8b62686ee5
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-11-09 18:01:20 +0700

    fix struc

--- lib/main.dart
+++ lib/main.dart
@@ -8,64 +8,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Skin Vault',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: Color(0xFF1e293b),      
-          onPrimary: Colors.white,         
-          secondary: Color(0xFFfcd34d),    
-          onSecondary: Color(0xFF1e293b),  
-          error: Color(0xFFef4444),        
-          onError: Colors.white,           
-          background: Color(0xFF212121),   
-          onBackground: Color(0xFF1e293b), 
-          surface: Colors.white,           
-          onSurface: Color(0xFF1e293b),    
-        ),
-        
-        // Konfigurasi tambahan tema
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFF1e293b),
-          foregroundColor: Colors.white,
-          elevation: 8,
-        ),
-        
-        // Konfigurasi kartu
-        cardTheme: CardTheme(
-          color: Colors.white,
-          elevation: 4,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-        
-        // Konfigurasi tombol
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFFfcd34d),
-            foregroundColor: Color(0xFF1e293b),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-          ),
-        ),
-        
-        // Konfigurasi teks
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            color: Color(0xFFfcd34d),
-            fontWeight: FontWeight.bold,
-          ),
-          bodyLarge: TextStyle(
-            color: Color(0xFF1e293b),
-          ),
-        ),
-        
-        // Menggunakan Material 3
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.deepPurple,
+        ).copyWith(secondary: Colors.deepPurple[400]),
         useMaterial3: true,
       ),
       home: MyHomePage(),
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
+class MyHomePage extends StatelessWidget {
+    final String npm = '2306152430'; // NPM
+    final String name = 'Malvin Scafi'; // Nama
+    final String className = 'PBP F'; // Kelas
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final String npm = '2306152430'; // NPM
-  final String name = 'Malvin Scafi'; // Nama
-  final String className = 'PBP F'; // Kelas
-
-  final List<ItemHomepage> items = [
-    ItemHomepage("Lihat Daftar Skin", Icons.gps_fixed, Colors.blue),
-    ItemHomepage("Tambah Skin", Icons.add, Colors.green),
-    ItemHomepage("Logout", Icons.logout, Colors.red),
+    final List<ItemHomepage> items = [
+      ItemHomepage("Lihat Daftar Skin", Icons.gps_fixed, Colors.blue),
+      ItemHomepage("Tambah Skin", Icons.add, Colors.green),
+      ItemHomepage("Logout", Icons.logout, Colors.red),
   ];
+
+    MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
       appBar: AppBar(
-        title: Text(
+        // Judul aplikasi "Skin Vault" dengan teks putih dan tebal.
+        title: const Text(
           'Skin Vault',
           style: TextStyle(
-            color: Theme.of(context).colorScheme.secondary,
+            color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -42,11 +45,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 InfoCard(title: 'Class', content: className),
               ],
             ),
+
+            // Memberikan jarak vertikal 16 unit.
             const SizedBox(height: 16.0),
+
+            // Menempatkan widget berikutnya di tengah halaman.
             Center(
               child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
                 children: [
-                  Padding(
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
+                  const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
                       'Welcome to Skin Vault',
@@ -56,13 +66,18 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ),
+
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
+
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -77,30 +92,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+
 class InfoCard extends StatelessWidget {
-  final String title;
-  final String content;
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title;  // Judul kartu.
+  final String content;  // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.5,
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
               title,
-              style: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.primary),
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8.0),
-            Text(
-              content,
-              style: TextStyle(color: Theme.of(context).colorScheme.primary),
-            ),
+            const SizedBox(height: 8.0),
+            Text(content),
           ],
         ),
       ),
@@ -109,49 +127,58 @@ class InfoCard extends StatelessWidget {
 }
 
 class ItemHomepage {
-  final String name;
-  final IconData icon;
-  final Color color;
+    final String name;
+    final IconData icon;
+    final Color color;
 
-  ItemHomepage(this.name, this.icon, this.color);
+    ItemHomepage(this.name, this.icon, this.color);
 }
 
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
 
-  const ItemCard(this.item, {super.key});
+
+class ItemCard extends StatelessWidget {
+  // Menampilkan kartu dengan ikon dan nama.
+
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
 
   @override
   Widget build(BuildContext context) {
     return Material(
+      // Menentukan warna latar belakang dari tema aplikasi.
       color: item.color,
+      // Membuat sudut kartu melengkung.
       borderRadius: BorderRadius.circular(12),
+      
       child: InkWell(
+        // Aksi ketika kartu ditekan.
         onTap: () {
+          // Menampilkan pesan SnackBar saat kartu ditekan.
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!")),
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
             );
         },
+        // Container untuk menyimpan Icon dan Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
+              // Menyusun ikon dan teks di tengah kartu.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
                   item.icon,
-                  color: Theme.of(context).colorScheme.secondary,
+                  color: Colors.white,
                   size: 30.0,
                 ),
                 const Padding(padding: EdgeInsets.all(3)),
                 Text(
                   item.name,
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
+                  style: const TextStyle(color: Colors.white),
                 ),
               ],
             ),
@@ -160,4 +187,5 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
+  
 }
